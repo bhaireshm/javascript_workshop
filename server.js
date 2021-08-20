4bf2555fca884fc1a491d124c127e1af69192c5a
Bhairesh M <bhairesh@arithaconsulting.com>
2021-08-20 12:32:17 +0530

    Update: Sample code and HP-API added.

---
+++ server.js
@@ -0,0 +1,34 @@
+const api_url = "http://hp-api.herokuapp.com/api/characters";
+
+async function fetchHarryPotterCharacters(data) {
+  // data initialise here
+  try {
+    // ternary, data object,
+
+    // shorthand
+    if (data) {
+      data = data;
+    } else {
+      data = {};
+    }
+
+    // shorthand
+    let url = data && data.url ? data.url : "";
+    if (data == null || data == undefined) {
+      data = data;
+    }
+
+    url = `${api_url}/${url}`;
+    console.info("URL:", url);
+
+    let response = await fetch(url);
+    if (response.ok) {
+      let json = await response.json(); // debug error here
+      return json;
+    } else {
+      console.error("HTTP-Error: " + response.status);
+    }
+  } catch (e) {
+    return e;
+  }
+}
